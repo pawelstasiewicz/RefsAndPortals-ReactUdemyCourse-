5a89edabbed224aa4ed1cc83f210aa230ed03661
pawelstasiewicz <pst357357@gmail.com>
2024-02-06 19:01:46 +0100

    Modify code in timer component

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,22 @@
-import { forwardRef } from "react"; 
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal(
+	{ result, targetTime },
+	ref
+) {
+
+    const dialog = useRef()
+
+	useImperativeHandle(ref,() =>{
+        return {
+            open(){
+                dialog.current.showModal()
+            }
+        }
+    });
+
 	return (
-		<dialog ref={ref} className="result-modal">
+		<dialog ref={dialog} className='result-modal'>
 			<h2>You {result}</h2>
 			<p>
 				The target time was <strong>{targetTime} second.</strong>
@@ -10,11 +24,11 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
 			<p>
 				You stopped the timer with <strong>X second left.</strong>
 			</p>
-			<form method="dialog">
-                <button>Close</button>
-            </form>
+			<form method='dialog'>
+				<button>Close</button>
+			</form>
 		</dialog>
 	);
-})
+});
 
 export default ResultModal;
